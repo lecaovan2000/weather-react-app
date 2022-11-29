ca72473a0e865050914e7ed308c8986f1e49306f
lecaovan2000 <lecaovan12102000@gmail.com>
2022-11-29 23:13:47 +0700

    hhihihi

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,48 +5,46 @@ import logoIuh from '../../logo-iuh.jpg';
 const Footer = () => {
     return (
         <Container className={styles.box}>
-            <Row>
-                <Col md={6} className={styles.content} style={{ alignItems: 'center', paddingTop: '25px' }}>
-                    <span>
-                        <img style={{ width: '200px' }} src={logoIuh} alt="#" />
-                    </span>
-                    <span>
-                        <p style={{ paddingTop: '10px' }}>Địa chỉ liên hệ: 12 Nguyễn Văn Bảo, P4 Gò Vấp, TP HCM </p>
-                    </span>
-                    <span>
-                        <p>Điện thoại: 0965 65 65 54 </p>
-                    </span>
-                    <span>
-                        <p>Email: thanhhunggit00@gmail.com </p>
-                    </span>
-                </Col>
-                <Col md={3} className={styles.content} style={{ textAlign: 'center', alignItems: 'center' }}>
-                    {' '}
-                    <p>Trang chủ</p>
-                    <p>Giới thiệu</p>
-                    <p>Mô hình</p>
-                    <p>Thiết bị</p>
-                    <p>Liệt kê</p>
-                </Col>
-                <Col md={3} className={styles.content} style={{ textAlign: 'center', alignItems: 'center' }}>
-                    <p>
-                        <a style={{ color: '#fff' }} href="/">
-                            Zalo
-                        </a>
-                    </p>
-                    <p>
-                        <a style={{ color: '#fff' }} href="https://www.facebook.com/profile.php?id=100076120746148">
-                            Facebook
-                        </a>
-                    </p>
+            <div className={styles.container}>
+                <Row>
+                    <Col md={6} className={styles.content} style={{ alignItems: 'center', paddingTop: '25px' }}>
+                        <span>
+                            <p style={{ paddingTop: '10px' }}>Địa chỉ liên hệ: 12 Nguyễn Văn Bảo, P4 Gò Vấp, TP HCM </p>
+                        </span>
+                        <span>
+                            <p>Điện thoại: 0965 65 65 54 </p>
+                        </span>
+                        <span>
+                            <p>Email: thanhhunggit00@gmail.com </p>
+                        </span>
+                    </Col>
+                    <Col md={3} className={styles.content} style={{ textAlign: 'center', alignItems: 'center' }}>
+                        {' '}
+                        <p>Trang chủ</p>
+                        <p>Giới thiệu</p>
+                        <p>Mô hình</p>
+                        <p>Thiết bị</p>
+                    </Col>
+                    <Col md={3} className={styles.content} style={{ textAlign: 'center', alignItems: 'center' }}>
+                        <p>
+                            <a style={{ color: '#fff' }} href="/">
+                                Zalo
+                            </a>
+                        </p>
+                        <p>
+                            <a style={{ color: '#fff' }} href="https://www.facebook.com/profile.php?id=100076120746148">
+                                Facebook
+                            </a>
+                        </p>
 
-                    <p>
-                        <a style={{ color: '#fff', right: '0' }} href="/">
-                            Instagram
-                        </a>
-                    </p>
-                </Col>
-            </Row>
+                        <p>
+                            <a style={{ color: '#fff', right: '0' }} href="/">
+                                Instagram
+                            </a>
+                        </p>
+                    </Col>
+                </Row>
+            </div>
         </Container>
     );
 };
